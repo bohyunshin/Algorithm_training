e75ad755b2ac8ef65e4a205b286495cb18b3f632
shinbo <shinbo0613@gmail.com>
2021-01-22 20:43:10 +0900

    숨바꼭질 마무리 못함...

--- BOJ/BFS/12851번 숨바꼭질2 (미완).py
+++ BOJ/BFS/12851번 숨바꼭질2 (미완).py
@@ -2,6 +2,7 @@ from collections import deque
 
 n,k = map(int, input().split())
 graph = [0]*100001
+ways = [0]*100001
 # 현재 수빈이의 위치를 표시해줌
 # graph[n] = 1
 # 좌,우,순간이동 좌표를 표시
@@ -9,38 +10,24 @@ def bfs(graph, start):
 
     queue = deque()
     queue.append(start)
-    min = []
-    count = 0
     while queue:
         x = queue.popleft()
-        # print(x)
 
-        # 만약에 수빈이가 위치한 곳이라면
-        # 바로 탈출
-        if x == k:
-            count += 1
-            min.append(graph[k])
-            graph[k] -= 1
-
+        # if x == k:
+        #     break
 
         nxs = [x-1, x+1, x * 2]
 
         # 인접한 세 좌표를 돈다
         for nx in nxs:
             # 주어진 범위를 벗어나는 경우는 제외
-            if nx < 0 or nx >= 100001:
-                continue
-            # 처음 가본 곳이라면
-            # 그곳으로 가보고 1을 더해줌
-            if graph[nx] == 0:
-                queue.append(nx)
-                graph[nx] = graph[x] + 1
-
-                # if nx == k:
-                #     count += 1
-                #     min.append(graph[nx])
-                #     graph[nx] = 0
-
-    return min, count
-
-print(bfs(graph,n))
+            if 0 <= nx < 100001:
+                # 처음 가본 곳이라면
+                # 그곳으로 가보고 1을 더해줌
+                if graph[nx] == 0 or graph[nx] >= graph[x]+1:
+                    queue.append(nx)
+                    ways[nx] += 1
+                    graph[nx] = graph[x] + 1
+    print(graph[k])
+    print(ways[k])
+bfs(graph,n)
