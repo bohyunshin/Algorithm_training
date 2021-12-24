c835b727d44e557a7cf81ff5bba02bb1496946e6
shinbo <shinbo0613@gmail.com>
2021-12-24 09:24:58 +0900

    20211224

--- BOJ/BF/test.py
+++ BOJ/BF/test.py
@@ -1,11 +1,20 @@
+m = 100000
+numbers = [1,2,3]
+dp = [[0]*4 for _ in range(m+1)]
+for j in numbers:
+    dp[j][j] = 1
+for i in range(1,m+1):
+    for j in range(1,4):
+        if i-j >= 0:
+            if j == 1:
+                dp[i][j] += dp[i-j][2] + dp[i-j][3]
+            if j == 2:
+                dp[i][j] += dp[i-j][1] + dp[i-j][3]
+            if j == 3:
+                dp[i][j] += dp[i-j][1] + dp[i-j][2]
+        dp[i][j] %= 1000000009
+
 t = int(input())
 for _ in range(t):
     n = int(input())
-    numbers = [1,2,3]
-    dp = [0]*(n+1)
-    dp[0] = 1
-    for i in range(1,n+1):
-        for j in range(1,4):
-            if i-j >= 0:
-                dp[i] += dp[i-j]
-    print(dp[n])
+    print(sum(dp[n]) % 1000000009)
--- BOJ/DP/15990번 1,2,3 더하기 5.py
+++ BOJ/DP/15990번 1,2,3 더하기 5.py
@@ -17,3 +17,24 @@ n = int(input())
 for _ in range(n):
     a = int(input())
     print(sum(dp[a]) % 1000000009)
+
+# m = 100000
+# numbers = [1,2,3]
+# dp = [[0]*4 for _ in range(m+1)]
+# for j in numbers:
+#     dp[j][j] = 1
+# for i in range(1,m+1):
+#     for j in range(1,4):
+#         if i-j >= 0:
+#             if j == 1:
+#                 dp[i][j] += dp[i-j][2] + dp[i-j][3]
+#             if j == 2:
+#                 dp[i][j] += dp[i-j][1] + dp[i-j][3]
+#             if j == 3:
+#                 dp[i][j] += dp[i-j][1] + dp[i-j][2]
+#         dp[i][j] %= 1000000009
+#
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     print(sum(dp[n]) % 1000000009)
