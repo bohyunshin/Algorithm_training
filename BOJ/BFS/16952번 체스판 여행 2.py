9d3bf5b6f7e3bbb5e8d141b5cbf9fa8adae3a915
shinbo <shinbo0613@gmail.com>
2022-01-06 23:50:29 +0900

    20220106

--- BOJ/BFS/16952번 체스판 여행 2.py
+++ BOJ/BFS/16952번 체스판 여행 2.py
@@ -8,7 +8,7 @@ class Piece:
         self.dist = dist
         self.change = change
 # 나이트, 비숍, 룩 순서로 3차원 dist 테이블 초기화
-visited = [[[[Piece(-1,-1)]*3 for _ in range(n*n)] for _ in range(n)] for _ in range(n)]
+visited = [[[[Piece(-1,1e100) for _ in range(3)] for _ in range(n*n)] for _ in range(n)] for _ in range(n)]
 # 몇번 말을 바꿨는지 기록하는 테이블 초기화
 # count = [[[[-1]*3 for _ in range(n*n)] for _ in range(n)] for _ in range(n)]
 q = deque()
@@ -24,7 +24,7 @@ for i in range(n):
                 # x좌표, y좌표, 현재 있는 번호, 어떤 말인지, 말을 몇번 바꿨는지.
                 q.append((i,j,0,k,0))
 ans = -1
-change = -1
+change = 1e100
 while q:
     x,y,num,horse,c = q.popleft()
     # 끝까지 간 경우
@@ -41,7 +41,7 @@ while q:
         if visited[x][y][num][i].dist == -1 or \
                 (visited[x][y][num][i].dist == visited[x][y][num][horse].dist + 1 and visited[x][y][num][i].change > c+1):
             visited[x][y][num][i].dist = visited[x][y][num][horse].dist + 1
-            visited[x][y][num][i].change = visited[x][y][num][horse].change + 1
+            visited[x][y][num][i].change = c + 1
             q.append((x,y,num,i,c+1))
     # 나이트라면
     if horse == 0:
@@ -56,7 +56,7 @@ while q:
                 if visited[nx][ny][next_num][horse].dist == -1 or \
                         (visited[nx][ny][next_num][horse].dist == visited[x][y][num][horse].dist+1 and visited[nx][ny][next_num][horse].change > c):
                     visited[nx][ny][next_num][horse].dist = visited[x][y][num][horse].dist+1
-                    visited[nx][ny][next_num][horse].change = visited[x][y][num][horse].change
+                    visited[nx][ny][next_num][horse].change = c
                     q.append((nx,ny,next_num,horse,c))
     # 비숍이라면, 굳이 끝까지 갈 필요는 없음.
     elif horse == 1:
@@ -71,7 +71,7 @@ while q:
                 if visited[nx][ny][next_num][horse].dist == -1 or \
                         (visited[nx][ny][next_num][horse].dist == visited[x][y][num][horse].dist+1 and visited[nx][ny][next_num][horse].change > c):
                     visited[nx][ny][next_num][horse].dist = visited[x][y][num][horse].dist+1
-                    visited[nx][ny][next_num][horse].change = visited[x][y][num][horse].change
+                    visited[nx][ny][next_num][horse].change = c
                     q.append((nx,ny,next_num,horse,c))
                 nx += dx[i]
                 ny += dy[i]
@@ -88,7 +88,7 @@ while q:
                 if visited[nx][ny][next_num][horse].dist == -1 or \
                         (visited[nx][ny][next_num][horse].dist == visited[x][y][num][horse].dist+1 and visited[nx][ny][next_num][horse].change > c):
                     visited[nx][ny][next_num][horse].dist = visited[x][y][num][horse].dist + 1
-                    visited[nx][ny][next_num][horse].change = visited[x][y][num][horse].change
+                    visited[nx][ny][next_num][horse].change = c
                     q.append((nx, ny, next_num, horse,c))
                 nx += dx[i]
                 ny += dy[i]
