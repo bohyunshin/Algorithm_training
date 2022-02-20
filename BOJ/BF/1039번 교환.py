9c4025d45c6f4b613d429d219d3ca1100329b18c
bohyunshin <shinbo0613@gmail.com>
2022-02-20 23:35:19 +0900

    20220220

--- BOJ/BF/1039번 교환.py
+++ BOJ/BF/1039번 교환.py
@@ -1,8 +1,28 @@
 from itertools import combinations
 from collections import deque
 n,k = input().split()
-m = len(n)
 k = int(k)
-locs = [i for i in range(m)]
+locs = [i for i in range(len(n))]
 q = deque()
-visited = [[-1]*k]
+visited = [[False]*1000001 for _ in range(k+1)]
+q.append((0,n))
+visited[0][int(n)] = False
+while q:
+    cnt,n = q.popleft()
+    if cnt == k:
+        continue
+    for c in combinations(locs,2):
+        x,y = c
+        if n[y] == '0' and x == 0:
+            continue
+        next_n = [i for i in n]
+        next_n[x], next_n[y] = next_n[y], next_n[x]
+        next_n = ''.join(next_n)
+        if visited[cnt+1][int(next_n)] == False:
+            visited[cnt+1][int(next_n)] = True
+            q.append((cnt+1,next_n))
+ans = -1
+for i in range(1000001):
+    if visited[k][i]:
+        ans = i
+print(ans)
--- BOJ/BF/16987번 계란으로 계란치기.py
+++ BOJ/BF/16987번 계란으로 계란치기.py
@@ -5,9 +5,15 @@ for _ in range(n):
     x,y = map(int,input().split())
     hard.append(x)
     weight.append(y)
+ans = 0
 def go(index,hard):
+    global ans
     if index == n:
-        print(hard)
+        tmp = 0
+        for i in range(n):
+            if hard[i] <= 0:
+                tmp += 1
+        ans = max(ans,tmp)
         return
     if hard[index] <= 0:
         go(index+1,hard.copy())
@@ -18,17 +24,13 @@ def go(index,hard):
             continue
         if hard[i] <= 0:
             cnt += 1
-    if cnt == n-1:
-        go(index+1,hard.copy())
-        return
-    for i in range(n):
-        if i == index:
-            continue
-        if hard[i] <= 0:
-            cnt += 1
             continue
         hard[index] -= weight[i]
         hard[i] -= weight[index]
         go(index+1,hard.copy())
-
+        hard[index] += weight[i]
+        hard[i] += weight[index]
+    if cnt == n-1:
+        go(index+1,hard.copy())
 go(0,hard)
+print(ans)
