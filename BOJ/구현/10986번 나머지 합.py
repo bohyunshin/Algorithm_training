c9b5e8933058b4110e46422cbf971ea6caf57497
bohyunshin <shinbo0613@gmail.com>
2022-03-09 23:57:02 +0900

    20220309

--- BOJ/구현/10986번 나머지 합.py
+++ BOJ/구현/10986번 나머지 합.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
-n,k = map(int,input().split())
+n,m = map(int,input().split())
 a = [0] + list(map(int,input().split()))
 s = [0]*(n+1)
-ans = 0
 dct = defaultdict(int)
+ans = 0
 for i in range(1,n+1):
-    s[i] = s[i-1] + a[i]
-    if s[i] == k:
+    s[i] = (s[i-1]%m + a[i]%m) % m
+    if s[i] % m == 0:
         ans += 1
-    ans += dct[s[i] - k]
     dct[s[i]] += 1
-print(ans)
+for mod in dct.keys():
+    v = dct[mod]
+    ans += v*(v-1)/2
+print(int(ans))
