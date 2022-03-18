35785922007d9c616e8d4d568ecdf11fc5e68016
bohyunshin <shinbo0613@gmail.com>
2022-03-18 23:38:28 +0900

    20220318

--- BOJ/BF/17281번 야구.py
+++ BOJ/BF/17281번 야구.py
@@ -4,65 +4,108 @@ innings = []
 ans = 0
 for _ in range(n):
     innings.append(list(map(int,input().split())))
-for p in permutations(range(9),9):
-    if p[3] != 0:
-        continue
-    # print(p)
+# candis = [i for i in range(1,9)]
+# for p in permutations(candis,len(candis)):
+#     # if p[3] != 0:
+#     #     continue
+#     p = list(p)
+#     p = p[:3] + [0] + p[3:]
+#     score = 0
+#     order = p[0]
+#     total_order = {}
+#     for i in range(len(p)):
+#         if i == len(p)-1:
+#             total_order[p[i]] = p[0]
+#         else:
+#             total_order[p[i]] = p[i+1]
+#     rest = [0]*3
+#     out = 0
+#     for inning in innings:
+#         while out <= 2:
+#             player = inning[order]
+#             # 아웃이면, 바로 아웃시키고 다음 타자 대기시킴.
+#             if player == 0:
+#                 out += 1
+#                 order = total_order[order]
+#                 continue
+#             # 아웃아니면 잔루주자 포함하여 타자들을 진루시킴.
+#             for i in range(len(rest) - 1, -1, -1):
+#                 if rest[i] == 0:
+#                     continue
+#                 if i+player >= len(rest):
+#                     score += 1
+#                 else:
+#                     rest[i+player] = 1
+#                 rest[i] = 0
+#             if player == 4:
+#                 score += 1
+#             else:
+#                 rest[player-1] = 1
+#             order = total_order[order]
+#         # 이닝 끝.
+#         out = 0
+#         rest = [0]*3
+#     ans = max(ans,score)
+# print(ans)
+# #
+
+def simulate():
     score = 0
-    order = p[0]
+    order = player[0]
     total_order = {}
-    for i in range(len(p)):
-        if i == len(p)-1:
-            total_order[p[i]] = p[0]
+    for i in range(len(player)):
+        if i == len(player) - 1:
+            total_order[player[i]] = player[0]
         else:
-            total_order[p[i]] = p[i+1]
-    rest = [0]*4
+            total_order[player[i]] = player[i + 1]
+    rest = [0] * 3
     out = 0
     for inning in innings:
         while out <= 2:
-            player = inning[order]
+            many = inning[order]
             # 아웃이면, 바로 아웃시키고 다음 타자 대기시킴.
-            if player == 0:
+            if many == 0:
                 out += 1
                 order = total_order[order]
                 continue
             # 아웃아니면 잔루주자 포함하여 타자들을 진루시킴.
             for i in range(len(rest) - 1, -1, -1):
-                rest[0] = 1
-                if i + player >= len(rest):
-                    if rest[i] == 1:
-                        score += 1
-                    rest[i] = 0
+                if rest[i] == 0:
+                    continue
+                if i + many >= len(rest):
+                    score += 1
                 else:
-                    rest[i + player] = rest[i]
-                    rest[i] = 0
-                rest[0] = 0
+                    rest[i + many] = 1
+                rest[i] = 0
+            if many == 4:
+                score += 1
+            else:
+                rest[many - 1] = 1
             order = total_order[order]
         # 이닝 끝.
         out = 0
-        # print(inning,order,score)
-    # print(rest,score)
-    # break
-    ans = max(ans,score)
-print(ans)
-# rest = [0,1,1,0]
-# player = 3
-# score = 0
-# out = 0
-# for i in range(len(rest)-1,-1,-1):
-#     if player == 0:
-#         out += 1
-#         continue
-#     rest[0] = 1
-#     if i+player >= len(rest):
-#         if rest[i] == 1:
-#             score += 1
-#         rest[i] = 0
-#     else:
-#         rest[i+player] = rest[i]
-#         rest[i] = 0
-#     rest[0] = 0
-# print(rest,score)
+        rest = [0] * 3
+    return score
 
+player = [0]*9
+check = [False]*9
+def go(index):
+    if index == 9:
+        return simulate()
+    if index == 3:
+        player[index] = 0
+        return go(index+1)
+    ans = 0
+    for i in range(1,9):
+        if check[i]:
+            continue
+        check[i] = True
+        player[index] = i
+        temp = go(index+1)
+        if ans < temp:
+            ans = temp
+        check[i] = False
+        player[index] = 0
+    return ans
 
-
+print(go(0))
