6a02fb92f459a7ef1ddef10342ee6c0a9c90c85f
bohyunshin <shinbo0613@gmail.com>
2022-05-02 23:53:24 +0900

    20220502

--- BOJ/DP/2629번 양팔저울.py
+++ BOJ/DP/2629번 양팔저울.py
@@ -4,13 +4,11 @@ m = int(input())
 b = list(map(int,input().split()))
 dp = [[0]*(n*500+1) for _ in range(n+1)]
 def go(idx,weight):
-    if idx > n:
-        return
-    print(idx, weight, dp[idx][weight])
     if dp[idx][weight]:
         return
-
     dp[idx][weight] = 1
+    if idx >= n:
+        return
     go(idx+1,weight)
     go(idx+1,weight + a[idx+1])
     go(idx+1,abs(weight - a[idx+1]))
@@ -19,7 +17,8 @@ for k in b:
     flag = False
     if k > 500*n:
         print('N', end=' ')
-    for i in range(n):
+        continue
+    for i in range(1,n+1):
         if dp[i][k]:
             flag = True
             break
@@ -27,5 +26,3 @@ for k in b:
         print('Y', end=' ')
     else:
         print('N', end=' ')
-for i in dp:
-    print(i)
